5b61b245e240f7a54b83bc6f52647838787779cd
mschangtaitai <mschangtaitai@gmail.com>
2022-03-17 22:57:17 -0600

    Error alert

--- main.py
+++ main.py
@@ -9,26 +9,34 @@ from afnSimulator import *
 er = input("Ingrese la expresion regular: ")
 w = input("Ingrese la cadena que desea verificar: ")
 
-afn = thompson(er)
-graphAF(afn, "AFN")
-start_time = time.time()
-afnTest = afnSimulator(afn,w)
-final_time = time.time() - start_time
-print("Cumple con el AFN: " + str(afnTest))
-print("Tiempo de validacion: " + str(final_time), end = "\n\n")
+try:
+    afn = thompson(er)
+    graphAF(afn, "AFN")
+    start_time = time.time()
+    afnTest = afnSimulator(afn,w)
+    final_time = time.time() - start_time
+    print("Cumple con el AFN: " + str(afnTest))
+    print("Tiempo de validacion: " + str(final_time), end = "\n\n")
 
-afd = subconjuntos(afn)
-graphAF(afd, "AFD")
-start_time = time.time()
-afdTest = afdSimulator(afd,w)
-final_time = time.time() - start_time
-print("Cumple con el AFD: " + str(afdTest))
-print("Tiempo de validacion: " + str(final_time), end = "\n\n")
+    afd = subconjuntos(afn)
+    graphAF(afd, "AFD")
+    start_time = time.time()
+    afdTest = afdSimulator(afd,w)
+    final_time = time.time() - start_time
+    print("Cumple con el AFD: " + str(afdTest))
+    print("Tiempo de validacion: " + str(final_time), end = "\n\n")
 
-afdmin = minAFD(afd)
-graphAF(afdmin, "AFDmin")
-start_time = time.time()
-afdminTest = afdSimulator(afdmin,w)
-final_time = time.time() - start_time
-print("Cumple con el AFD minimizado: " + str(afdminTest))
-print("Tiempo de validacion: " + str(final_time), end = "\n\n")
+    afdmin = minAFD(afd)
+    graphAF(afdmin, "AFDmin")
+    start_time = time.time()
+    afdminTest = afdSimulator(afdmin,w)
+    final_time = time.time() - start_time
+    print("Cumple con el AFD minimizado: " + str(afdminTest))
+    print("Tiempo de validacion: " + str(final_time), end = "\n\n")
+except:
+    print('''
+**********************************************************************************************
+                    Ha ocurrido un problema procesando el programa!
+            Revisa que estes ingresando de manera correcta la expresion regular!
+**********************************************************************************************
+''')
